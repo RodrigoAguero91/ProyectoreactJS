6234b7ec1293f1107b6711571d9be0b38884883e
Rodrigo Aguero <aguerorodrigo91@gmail.com>
2023-12-08 19:58:35 -0300

    asdf

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,20 @@
 import { useContext, useState } from "react";
 import { Container, Table, Button, Form } from "react-bootstrap";
 import { CartContext } from "../contexts/CartContext";
+import {
+    getFirestore,
+    collection,
+    addDoc,
+  } from "firebase/firestore";
 
-export const Cart = () => {
-  const [buyer, setbuyer] = useState({
+const initialValues = {
     email: "",
     name: "",
     phone: "",
-  });
+};
+
+export const Cart = () => {
+  const [buyer, setbuyer] = useState(initialValues);
   const { clear, items, onRemove } = useContext(CartContext);
 
   const handleChange = (event) => {
@@ -17,7 +24,27 @@ export const Cart = () => {
            [event.target.name]: event.target.value,
         };
     });
-  }
+  };
+
+  const sendOrder = () => {
+    const order = {
+        buyer,
+        items,
+        total: 13600,
+      };
+  
+      const db = getFirestore();
+      const orderCollection = collection(db, "orders");
+  
+      addDoc(orderCollection, order).then(({ id }) => {
+        if (id) {
+          alert("Su orden: " + id + " ha sido completada!");
+          setbuyer(initialValues);
+          clear();
+        }
+      });
+    
+  };
 
   return (
     <Container className="mt-4">
@@ -62,7 +89,7 @@ export const Cart = () => {
           <Form.Control type="text" value={buyer.phone} onChange={handleChange}name="phone" />
         </Form.Group>
 
-        <Button variant="primary">ENVIAR</Button>
+        <Button variant="primary" onClick={sendOrder}>ENVIAR</Button>
       </Form>
     </Container>
   );
